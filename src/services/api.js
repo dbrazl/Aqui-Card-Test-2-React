b0e08da09d3af51f5bebdef102f2129a5fd2a198
dbrazl <danielbrazlopes16@gmail.com>
2020-01-27 09:00:55 -0300

    Change endpoint

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const api = axios.create({
   baseURL:
-    'http://aquicardtest2-env.b24mpccuqa.us-east-1.elasticbeanstalk.com/:80',
+    'http://aquicardtest2-env.b24mpccuqa.us-east-1.elasticbeanstalk.com:80',
 });
 
 export default api;
